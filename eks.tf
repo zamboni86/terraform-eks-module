767484df430d17ee0bc9910e27d51cacb62d3e98
zamboni86 <zanoni.contreras@gmail.com>
2024-05-01 22:39:18 -0600

    chore: rename files

--- eks.tf
+++ eks.tf
@@ -1,21 +1,3 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "example-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
-  public_subnets  = ["10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19"]
-
-  enable_nat_gateway = true
-
-  tags = {
-    terraform = "true"
-    environment = "dev"
-  }
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
