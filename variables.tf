e25b496f3d766612098bff9bfd49a8e6ff644313
zamboni86 <zanoni.contreras@gmail.com>
2024-05-04 11:47:08 -0600

    add public subnets back

--- variables.tf
+++ variables.tf
@@ -11,8 +11,12 @@ variable "env" {
     default = "dev"
 }
 
-variable "subnets" {
+variable "private_subnets" {
   default = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
+}
+
+variable "public_subnets" {
+  default = ["10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19"]
 }
 
 variable "tags" {
