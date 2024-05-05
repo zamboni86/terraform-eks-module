1ea95a32db2111f4fb80480d31bb0ac6e2730514
zamboni86 <zanoni.contreras@gmail.com>
2024-05-05 01:50:30 -0600

    new

--- eks.tf
+++ eks.tf
@@ -60,7 +60,7 @@ module "eks" {
 
       policy_associations = {
         example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
             namespaces = []
             type       = "cluster"
